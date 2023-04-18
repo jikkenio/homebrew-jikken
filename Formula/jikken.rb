aadba18c6e8b9f980c2860d48f0db2b27d7f1982
Louis Russo <louis@jikken.io>
2023-04-18 10:53:42 -0500

    update homebrew to release v0.5.0

--- Formula/jikken.rb
+++ Formula/jikken.rb
@@ -1,14 +1,14 @@
 class Jikken < Formula
-  version "0.4.0"  
+  version "0.5.0"  
   desc "API Automation Testing Tool written in Rust"
   homepage "https://github.com/jikkenio/jikken"
   
   if OS.mac?
-    url "https://github.com/jikkenio/jikken/releases/download/v0.4.0/jikken-v0.4.0-x86_64-apple-darwin.tar.gz"
-    sha256 "3347d0785a78c4eb0553393b21ccea754da6be02ef5b29d5d087ed4190aafd69"
+    url "https://github.com/jikkenio/jikken/releases/download/v0.5.0/jikken-v0.5.0-x86_64-apple-darwin.tar.gz"
+    sha256 "72977ec2260e4560010652dcf00aa104e33ada167c76efc941d016737a62a934"
   elsif OS.linux?
-    url "https://github.com/jikkenio/jikken/releases/download/v0.4.0/jikken-v0.4.0-x86_64-unknown-linux-musl.tar.gz"
-    sha256 "1d6bebadefa05d5a987795b7fbf1ca2ffe2d47e43bceca0ac9184906d7801172"
+    url "https://github.com/jikkenio/jikken/releases/download/v0.5.0/jikken-v0.5.0-x86_64-unknown-linux-musl.tar.gz"
+    sha256 "8bf4f0d7fe6eccf390f76913d5b49308ba15d6a8f2c6dc0bf6583157820f6767"
   end
 
   def install
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # homebrew-jikken
+
+**NOTE** The SHA256 for Homebrew Formula is the SHA256 of the ARCHIVE and not of the binary INSIDE the archive.
