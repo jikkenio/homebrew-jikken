a83abe053baa0406bbb404cf09012b19cee65b12
Louis Russo <louis@jikken.io>
2024-01-13 18:30:44 -0600

    fix linuxbrew reference for 0.6.0

--- Formula/jikken.rb
+++ Formula/jikken.rb
@@ -7,7 +7,7 @@ class Jikken < Formula
     url "https://github.com/jikkenio/jikken/releases/download/v0.6.0/jikken-v0.6.0-x86_64-apple-darwin.tar.gz"
     sha256 "4201bfb162b3c0ceb8c6948a9be321ce07bd8c0f3daac7fef5bce251a4ac6f86"
   elsif OS.linux?
-    url "https://github.com/jikkenio/jikken/releases/download/v0.6.0/jikken-v0.5.0-x86_64-unknown-linux-musl.tar.gz"
+    url "https://github.com/jikkenio/jikken/releases/download/v0.6.0/jikken-v0.6.0-x86_64-unknown-linux-musl.tar.gz"
     sha256 "eea241a24bf794c2da470189f7862882af540947736ba9b701873bebe992ec2e"
   end
 
