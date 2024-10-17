738bb50e037aea83a07ac73a11640efa763b0194
Louis Russo <louis@jikken.io>
2024-10-17 09:16:53 -0500

    Update release to V0.8.1

--- Formula/jikken.rb
+++ Formula/jikken.rb
@@ -1,14 +1,14 @@
 class Jikken < Formula
-  version "0.8.0"
+  version "0.8.1"
   desc "API Automation Testing Tool written in Rust"
   homepage "https://github.com/jikkenio/jikken"
   
   if OS.mac?
-    url "https://github.com/jikkenio/jikken/releases/download/v0.8.0/jikken-v0.8.0-x86_64-apple-darwin.tar.gz"
-    sha256 "cc57c67a0886e05f1e9aa5426134b6d639005b326ed78d0e64a5d90e9876c904"
+    url "https://github.com/jikkenio/jikken/releases/download/v0.8.1/jikken-v0.8.1-x86_64-apple-darwin.tar.gz"
+    sha256 "55cd5e2e1861133b837b5f7bad0aacb9198946bed2be6fd9cc6db8d65313af10"
   elsif OS.linux?
-    url "https://github.com/jikkenio/jikken/releases/download/v0.8.0/jikken-v0.8.0-x86_64-unknown-linux-musl.tar.gz"
-    sha256 "aab6d832c9de8d136ee3833da5d2b9d97ce6ae610118edd8e1c917a09eb7c877"
+    url "https://github.com/jikkenio/jikken/releases/download/v0.8.1/jikken-v0.8.1-x86_64-unknown-linux-musl.tar.gz"
+    sha256 "ded7f382401d7f21d3af6f2196838c981ae2f1169ccd0fa906b170b3e58a4913"
   end
 
   def install
