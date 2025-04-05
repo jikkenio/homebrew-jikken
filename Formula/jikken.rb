d3a1aa43c55f70d187bb52718ac2aafd62ce7d4b
Louis Russo <louis@jikken.io>
2025-04-05 12:16:35 -0500

    Release Jikken 0.8.2

--- Formula/jikken.rb
+++ Formula/jikken.rb
@@ -1,14 +1,21 @@
 class Jikken < Formula
-  version "0.8.1"
+  version "0.8.2"
   desc "API Automation Testing Tool written in Rust"
   homepage "https://github.com/jikkenio/jikken"
   
   if OS.mac?
-    url "https://github.com/jikkenio/jikken/releases/download/v0.8.1/jikken-v0.8.1-x86_64-apple-darwin.tar.gz"
-    sha256 "55cd5e2e1861133b837b5f7bad0aacb9198946bed2be6fd9cc6db8d65313af10"
+    if `uname -m`.chomp == "arm64"
+      # Apple Silicon specific code
+      url "https://github.com/jikkenio/jikken/releases/download/v0.8.2/jikken-v0.8.2-develop-aarch64-apple-darwin.tar.gz"
+      sha256 "cfb71ad3d45953b3500ada06be5b42cb496388e5d57546a297af16fa235608f6"
+    else
+      # Intel specific code
+      url "https://github.com/jikkenio/jikken/releases/download/v0.8.2/jikken-v0.8.2-develop-x86_64-apple-darwin.tar.gz"
+      sha256 "d54dbba1b8556f6a3d93f36cf96e7df2699a46109e84e28166b80ec0aa50a58a"
+    end
   elsif OS.linux?
-    url "https://github.com/jikkenio/jikken/releases/download/v0.8.1/jikken-v0.8.1-x86_64-unknown-linux-musl.tar.gz"
-    sha256 "ded7f382401d7f21d3af6f2196838c981ae2f1169ccd0fa906b170b3e58a4913"
+    url "https://github.com/jikkenio/jikken/releases/download/v0.8.2/jikken-v0.8.2-develop-x86_64-unknown-linux-musl.tar.gz"
+    sha256 "bb823c16060235f480db91761356f0c7c9078fff8b742e3bcb0fba6ed8d43996"
   end
 
   def install
